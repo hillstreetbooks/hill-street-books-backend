654dae64640fe98b080c5792a0d1165cb51f534c
Gokul <gokulpulari@gmail.com>
2022-01-31 23:59:09 -0500

    [HSB-12] Connect the node server with the cloud DB

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const path = require('path');
 
+const dotenv = require('dotenv');
 const express = require('express');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+const { MONGO_URI, PORT } = process.env;
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log('listening on port ', PORT);
+    app.listen(PORT);
+  })
+  .catch((err) => console.log(err));
 
 module.exports = app;
