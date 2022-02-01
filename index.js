9e39c0b0ba73ced0b6b9b00bbb321f16620e524c
Gokul <gokulpulari@gmail.com>
2022-02-01 00:11:24 -0500

    [HSB-14] Configure CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import { errorHandler } from './src/middleware/ErrorHandler.js';
 
 dotenv.config();
@@ -9,6 +10,8 @@ const { MONGO_URI, PORT } = process.env;
 
 const app = express();
 
+//Enable cors
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(errorHandler);
